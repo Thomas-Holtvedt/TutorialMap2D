7843a73c566fee99ba09237ec78fd40047abc3a7
René van der Ark <r.van.der.ark@gmail.com>
2026-08-08 23:15:54 +0200

    add hole support.

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 @onready var map_image: Sprite2D = $Sprite2D
 
-
 func _ready():
 	load_regions()
+
 
 func load_regions():
 	var image := map_image.texture.get_image()
@@ -12,22 +12,22 @@ func load_regions():
 	var regions_dict = import_file("res://Map_data/regions.txt")
 	if regions_dict == null:
 		return
-	
+
 	for region_color: String in regions_dict:
 		var region := load("res://Scenes/Region_Area.tscn").instantiate() as RegionArea
 		region.region_name = regions_dict[region_color]
 		region.name = region_color
 		get_node("Regions").add_child(region)
-		
-		var polygons := get_polygons(image, region_color, pixel_color_dict)
-		
+
+		var polygons := get_polygons(region_color, pixel_color_dict)
+
 		for polygon in polygons:
 			var region_collision = CollisionPolygon2D.new()
 			var region_polygon = Polygon2D.new()
-			
+
 			region_collision.polygon = polygon
 			region_polygon.polygon = polygon
-			
+
 			region.add_child(region_collision)
 			region.add_child(region_polygon)
 	map_image.queue_free()
@@ -42,15 +42,17 @@ func get_pixel_color_dict(image: Image) -> Dictionary[String, Array]:
 	return pixel_color_dict
 
 ## Return polygons for the region of a specific color in the image.
-func get_polygons(image: Image, region_color: String, pixel_color_dict: Dictionary[String, Array]) -> Array[PackedVector2Array]:
-	var target_image := Image.create_empty(image.get_size().x, image.get_size().y, false, Image.FORMAT_RGBA8)
-	for value in pixel_color_dict[region_color]:
-		target_image.set_pixelv(value, Color.WHITE)
-	
-	var bitmap = BitMap.new()
-	bitmap.create_from_image_alpha(target_image)
-	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0,0), bitmap.get_size()), 0.1)
-	return polygons
+func get_polygons(region_color: String, pixel_color_dict: Dictionary[String, Array]) -> Array[PackedVector2Array]:
+	var pixel_polys : Array[PackedVector2Array] = []
+	for pixel_point in pixel_color_dict[region_color]:
+		pixel_polys.append(PackedVector2Array([
+			Vector2(pixel_point),
+			Vector2(pixel_point) + Vector2.RIGHT,
+			Vector2(pixel_point) + Vector2.DOWN + Vector2.RIGHT,
+			Vector2(pixel_point) + Vector2.DOWN
+		]))
+	return Geometry2DUtil.union_polygons(pixel_polys)
+
 
 ## Import JSON files and converts to lists or dictionary.
 func import_file(filepath: String):
